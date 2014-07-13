f25d5e14e906da748cf1f9564899c04d72a6c652
199lane <yuan.aiqing@gmail.com>
2014-07-13 15:13:49 +0800

    init

---
+++ MediumERP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MediumERP.Startup))]
+namespace MediumERP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
